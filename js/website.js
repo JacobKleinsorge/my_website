24e3cf5f2d29fed83879e07e0378cb3fe9727397
Jacob Kleinsorge <jacobkleinsorge@gmail.com>
2017-10-10 13:05:57 -0400

    Added the crashed code

--- js/website.js
+++ js/website.js
@@ -1,4 +1,11 @@
-//$(document).ready(function() {
-//        var navHeight = $('#nav').height();
-//        $('body').css("padding-top", navHeight);
-//    });
+$(document).ready(function() {
+    var navHeight = $('#nav').height();
+    $('body').css("padding-top", navHeight+15);
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-nav').children.removeClass('active');
+        $(this.parent).addClass('active');
+    });
+});
